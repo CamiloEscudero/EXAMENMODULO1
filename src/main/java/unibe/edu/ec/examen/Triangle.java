cd83fb0eee4b3bfa88d6a8ee9548ff44bccf453a
camil <camil@DESKTOP-4GCMOE7>
2020-06-03 10:29:14 -0500

    Modify class triangle

--- src/main/java/unibe/edu/ec/examen/Triangle.java
+++ src/main/java/unibe/edu/ec/examen/Triangle.java
@@ -1,5 +1,19 @@
 package unibe.edu.ec.examen;
 
 public class Triangle {
+	private double side;
+	
+	public Triangle(double  side) {
+		super();
+		this.side = side;
+	}
+
+	public double getSide() {
+		return side;
+	}
+
+	public void setSide(double side) {
+		this.side = side;
+	}
 
 }
